571eb5dd6354545b7dda880cb095b41cce13013e
kubersharma001 <kubersharma001@gmail.com>
2021-11-02 20:47:40 +0530

    phoenix: props: Set frame rate multiple threshold to 120
    Also fixes the Netflix 120hz stuck issue as mentioned
    on Change-Id: Icdd392332a6f44d5d1b2dc1e6e654b11f9e95da2
    
    test: Play a Netflix or YT video; it goes down to lowest refresh rate.
    
    Change-Id: Ie4d5c8c9a45079306ad51e1607ae72eb9493ee0c

--- default.mk
+++ default.mk
@@ -23,6 +23,7 @@
 # https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    debug.sf.frame_rate_multiple_threshold=120 \
     debug.sf.use_phase_offsets_as_durations=1 \
     debug.sf.late.sf.duration=10500000 \
     debug.sf.late.app.duration=20500000 \
